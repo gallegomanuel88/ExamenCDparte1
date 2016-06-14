ab8556302d73480eb26abcc3d0481e3482c49242
galle <galle@DESKTOP-J6RSNG6>
2016-06-14 20:52:51 +0200

    - String cambiado en los System.Out.Println

--- src/examenfinalparte1/ImpresionResultados.java
+++ src/examenfinalparte1/ImpresionResultados.java
@@ -7,14 +7,14 @@ public class ImpresionResultados {
     public static void main(String[] args) throws IOException {
         Operaciones suma = new Operaciones(10, 20);
         int resultadoSuma = suma.resultado;
-        System.out.println("Es: " + resultadoSuma);
+        System.out.println("El resultado es: " + resultadoSuma);
         
         Operaciones multiplicacionX2 = new Operaciones(10);
         int resultadoMulti = multiplicacionX2.resultado;
-        System.out.println("Es: " + resultadoMulti);
+        System.out.println("El resultado es: " + resultadoMulti);
         
         Operaciones valorFijo = new Operaciones();
         int resultadoValorFijo = valorFijo.resultado;
-        System.out.println("Es: " + resultadoValorFijo);
+        System.out.println("El resultado es: " + resultadoValorFijo);
     }
 }
